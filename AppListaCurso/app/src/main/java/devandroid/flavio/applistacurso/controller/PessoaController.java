15febde0cc36fcef1b909abe357738749287162d
fbfracalossi <fracalossi@gmail.com>
2023-06-10 23:00:47 -0300

    Implementação da camada de Controller MVC

--- AppListaCurso/app/src/main/java/devandroid/flavio/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/flavio/applistacurso/controller/PessoaController.java
@@ -1,4 +1,16 @@
 package devandroid.flavio.applistacurso.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 public class PessoaController {
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        Log.d("MVC_Controller", "Controller iniciada...");
+        return super.toString();
+    }
 }
