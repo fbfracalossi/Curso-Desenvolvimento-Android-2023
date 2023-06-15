d0163d167768e75755b2b61e81af9e76e1ef66ff
fbfracalossi <fracalossi@gmail.com>
2023-06-15 18:03:20 -0300

    SharedPreferences Buscar Dados Salvos

--- AppListaCurso/app/src/main/java/devandroid/flavio/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/flavio/applistacurso/view/MainActivity.java
@@ -17,6 +17,7 @@ import devandroid.flavio.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
     public static final String NOME_PREFERENCES = "pref_listavip";
     PessoaController controller;
     Pessoa pessoa;
@@ -38,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         preferences = getSharedPreferences(NOME_PREFERENCES, 0);
-        SharedPreferences.Editor listaVip = preferences.edit();
+        listaVip = preferences.edit();
         controller = new PessoaController();
         controller.toString();
         pessoa = new Pessoa();
@@ -69,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
                 editSobrenomeAluno.setText("");
                 editTelefoneContato.setText("");
                 editNomeCurso.setText("");
+                listaVip.clear();
+                listaVip.apply();
             }
         });
 
